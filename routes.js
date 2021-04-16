29adfcec10a5ec42b72c5f8f4011860f8405431f
Jean Carlos <jean-449@hotmail.com>
2021-04-16 20:05:57 -0300

    until button Delete

--- routes.js
+++ routes.js
@@ -16,9 +16,14 @@ routes.get('/instructors/create', function(req, res) {
 
 routes.get('/instructors/:id', instructors.show)
 
-routes.get('/instructors/:id/edit', instructors.edit)
+routes.get('/instructors/:id/edit', instructors.edit) 
+
 
 routes.post('/instructors', instructors.post)
+
+routes.put("/instructors", instructors.put)
+
+routes.delete("/instructors", instructors.delete)
 
 routes.get('/members', function(req, res) {
     return res.send("members")
